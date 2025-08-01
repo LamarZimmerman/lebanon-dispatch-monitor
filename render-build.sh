8d7560b70f252ffa74e8eb626b51c8a9458c9b43
LamarZimmerman <lamarzimmey02@gmail.com>
2025-08-01 19:04:40 -0400

    Switch to puppeteer-core and install Chromium dependencies

--- render-build.sh
+++ render-build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y chromium
+apt-get update && apt-get install -y chromium ca-certificates fonts-liberation libappindicator3-1 libasound2 libatk-bridge2.0-0 \
+libatk1.0-0 libcups2 libdbus-1-3 libgdk-pixbuf2.0-0 libnspr4 libnss3 libx11-xcb1 libxcomposite1 libxdamage1 libxrandr2 \
+xauth xvfb
+
 echo "✅ Chromium installed at: $(which chromium)"
