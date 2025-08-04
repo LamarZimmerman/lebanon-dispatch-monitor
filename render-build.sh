483172f3c03012feeeef47e46c708a3cf2337d26
LamarZimmerman <lamarzimmey02@gmail.com>
2025-08-04 06:59:34 -0400

    Puppeteer replace

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-# Backend is in root
+set -e
+
+# Install Chromium for puppeteer-core
+apt-get update
+apt-get install -y chromium
+
+# Install Node dependencies
 npm install
